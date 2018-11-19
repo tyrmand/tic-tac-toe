315b50b44e6d3122b733d11a1c6f6a9f591f21cd
Michał Surmacz <michals.git@gmail.com>
2018-11-19 21:16:10 +0100

    Rewrites the Square component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button 
+function Square(props) {
+    return (
+        <button
             className="square"
-            onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        )
-    }
+            onClick={props.onClick()}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -26,16 +24,16 @@ class Board extends React.Component {
     handleClick(i) {
         const squares = this.state.squares.slice();
         squares[i] = "X";
-        this.setState ({
+        this.setState({
             squares: squares
         })
     }
 
     renderSquare(i) {
-        return <Square 
+        return <Square
             value={this.state.squares[i]}
             onClick={() => this.handleClick(i)}
-            />;
+        />;
     }
 
     render() {
@@ -47,17 +45,17 @@ class Board extends React.Component {
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
-                    {this.renderSquare(2)}                    
+                    {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
-                    {this.renderSquare(5)}                    
+                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
-                    {this.renderSquare(8)}                    
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
@@ -66,17 +64,17 @@ class Board extends React.Component {
 
 class Game extends React.Component {
     render() {
-       return (
-           <div className="game">
-            <div className="game-board">
-                <Board />
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
                 </div>
                 <div className="game-info">
-                <div>{/*status */}</div>
-                <ol>{/* TODO */}</ol>
+                    <div>{/*status */}</div>
+                    <ol>{/* TODO */}</ol>
                 </div>
-           </div>
-       ) 
+            </div>
+        )
     }
 }
 
