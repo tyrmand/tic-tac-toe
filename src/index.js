e9007bcb60091a12cf8c20208ab66953a198c4db
Michał Surmacz <michals.git@gmail.com>
2018-11-20 18:59:21 +0100

    Introduces taking turns by changing the Board state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button
-            className="square"
-            onClick={props.onClick()}
-        >
+        <button className="square" onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -17,16 +14,18 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = "X";
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            squares: squares
-        })
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -37,7 +36,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
